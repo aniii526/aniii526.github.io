742bbe6c249f538f6d64e5446c6139bae9ccd9a0
aniii526 <ani09pro@gmail.com>
2016-11-17 12:46:45 +0300

    qweqweqwe

--- panel/panel_slot_web.ts
+++ panel/panel_slot_web.ts
@@ -459,7 +459,7 @@ class PanelSlotMob extends PanelSlotWeb {
         this.preloader.position.x = Math.round(1170 / 2);
         this.preloader.position.y = Math.round(623 / 2);
         this.preloader.play();
-        this.addChild(this.preloader);
+        //this.addChild(this.preloader);
 
         this.containerGame = new PIXI.Sprite();
         this.addChild(this.containerGame);
