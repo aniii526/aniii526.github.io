4a44b3c9cfa7dfe053f0f117995a247ee79db0d0
aniii526 <ani09pro@gmail.com>
2016-12-15 10:38:51 +0300

    123123123123

--- all.js
+++ all.js
@@ -141,8 +141,8 @@ var pd = function(e) {
     };
     a.check = function() {
         var a = window.screen.width, d = window.screen.height, e = Math.min(a, d), a = Math.max(a, d), d = !1;
-        /*var str = e + " " + window.screen.availHeight + " " + window.innerHeight;
-        console.log(str);*/
+        //var str = e + " " + window.screen.availHeight + " " + window.innerHeight;
+        console.log(Math.abs(e - Math.min(window.screen.availHeight, window.innerHeight)));
         90 == Math.abs(window.orientation) && 0 < Math.abs(e - Math.min(window.screen.availHeight, window.innerHeight)) && (d=!0, this.eb.style.width =
         y("%dpx", a), this.eb.style.height = y("%dpx", e + 100));
         d ? (this.eb.style.display = "block", sd=!0) : (myResize(), this.eb.style.display = "none" , sd=!1)
